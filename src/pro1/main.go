0cc24f576633951a4d198c7401d6bb1f0b05b3bb
yanlele <331393627@qq.com>
2023-06-28 23:27:25 +0800

    feat: 添加依赖包

--- src/pro1/main.go
+++ src/pro1/main.go
@@ -1,44 +1,16 @@
 package main
 
 import (
-	"flag"
+	"go-project-demo/src/pro1/cmd"
 	"log"
 )
 
 var name string
 
 func main() {
-	flag.Parse()
+	err := cmd.Execute()
 
-	goCmd := flag.NewFlagSet("go", flag.ExitOnError)
-
-	goCmd.StringVar(&name, "name", "golang", "help - golang")
-
-	phpCmd := flag.NewFlagSet("php", flag.ExitOnError)
-
-	phpCmd.StringVar(&name, "n", "php", "help - php")
-
-	args := flag.Args()
-
-	if len(args) <= 0 {
-		return
+	if err != nil {
+		log.Fatalf("cmd.Execute err : %v\n", err)
 	}
-
-	switch args[0] {
-	case "go":
-		{
-			_ = goCmd.Parse(args[1:])
-			break
-		}
-	case "php":
-		{
-			_ = phpCmd.Parse(args[1:])
-			break
-		}
-	default:
-		name = ""
-		break
-	}
-
-	log.Printf("name: %s", name)
 }
